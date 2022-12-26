6bd5439a3b5d752732a1bf625b32bb08c97517a9
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2022-12-26 08:42:40 +0300

    delete

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var _createClass = function () { function defineProperties(target, props) { for 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var Balls = function () {
-    function Balls(context, buffer) {
+  function Balls(context, buffer) {
         _classCallCheck(this, Balls);
 
         this.context = context;
